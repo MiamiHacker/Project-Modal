9b386f22d615c032f8b4bc25b1fab3b8c0577784
MiamiHacker <105590216+MiamiHacker@users.noreply.github.com>
2022-07-18 07:29:01 -0400

    outline console.log

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
+//  console.log(btnsOpenModal);
 
 const openModal = function () {
   console.log(`Button Clicked`);
